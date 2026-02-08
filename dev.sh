3ea35e2cc5dc8722be13ce8555822c7b01c39d4d
Daniel Franklin <daniel@danielzfranklin.org>
2026-02-08 07:05:41 +0000

    Add tilejsons and dev viewer

--- dev.sh
+++ dev.sh
@@ -1,8 +1,2 @@
 #!/usr/bin/env bash
-# if arg --prod-server is set, do not set DEV_SERVER
-if [ "$1" == "--prod-server" ]; then
-    dev_server_arg=""
-else
-    dev_server_arg="--env DEV_SERVER=1"
-fi
-docker-compose run --build --rm --remove-orphans --service-ports $dev_server_arg dev
+docker compose up --build --remove-orphans dev dev_viewer
